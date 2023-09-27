a17a6ced224d786f044d9db1bf7a65989bf629b8
vermasuraj11011 <vermasuraj0008@gmail.com>
2023-09-27 10:30:12 +0530

    adding unit test case for the get by id function in user controller

--- src/test/java/com/specification/UserControllerTest.java
+++ src/test/java/com/specification/UserControllerTest.java
@@ -14,7 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -26,25 +28,30 @@ public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    public void addUser() {
-
-    }
-
+    Role mockRole = new Role(1, "admin");
+    User mockUser = new User(1, "suraj@gmail.com", "Suraj Verma", 28, mockRole, null);
 
     @Test
-    public void test_findAllUsers() throws Exception {
-        Role mockRole = new Role(1, "admin");
-
-        User mockUser = new User(1, "suraj@gmail.com", "Suraj Verma", 28, mockRole, null);
-
+    public void test_addUser() throws Exception {
         when(userService.create(mockUser)).thenReturn(mockUser);
         mockMvc.perform(
                         post("/user/")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(new ObjectMapper().writeValueAsString(mockUser)))
                 .andExpect(status().isOk());
+    }
 
+
+    @Test
+    public void test_getUser() throws Exception {
+        when(userService.getUser(mockUser.getId())).thenReturn(mockUser);
+        mockMvc.perform(
+                        get("/user/{id}", mockUser.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(null)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Suraj Verma"))
+                .andExpect(jsonPath("$.role.type").value("admin"));
     }
 
 }
